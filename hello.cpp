bf2edcc6566f6fe3bd54adbbe946c3a0b3706ff0
Fardeen <fardeenclan@gmail.com>
2025-03-13 16:09:06 +0530

    retartness-100

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "The CC-Lab-8 is most retarted lab document ever made" << endl;
   return 0;
 }
